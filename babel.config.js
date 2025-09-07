9841e1038f0bccf80558ea8497b0b415c880ef1b
theta-prog <yudengdaotian@gmail.com>
2025-09-07 15:15:11 +0900

    fix babel config

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo',
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      '@babel/preset-typescript',
+  'babel-preset-expo',
+  '@babel/preset-typescript',
     ],
     plugins: [
       'react-native-reanimated/plugin',
